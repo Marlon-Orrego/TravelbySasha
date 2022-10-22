720604bec109dc1a539d5a940e9ace4aadebbcf2
Marlon-Orrego <55565109+Marlon-Orrego@users.noreply.github.com>
2022-10-21 19:22:14 -0500

    FrontEnd modificación

--- desarrollo-web-backend-TravelbySasha/src/controllers/MongoDb.js
+++ desarrollo-web-backend-TravelbySasha/src/controllers/MongoDb.js
@@ -6,6 +6,197 @@ const { MongoClient, ObjectId } = require('mongodb')
 // HOST: clusterudem.3l9e6.mongodb.net
 // USER: saurmo-udem
 // PASSWORD: 9nVhp5fsbdKQRBLf
-// DATABASE: tienda
+// DATABASE: SASHA
 
 
+//Vuelo-Marlon
+const getVuelos = async (dbName, collectionName) => {
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.find({}).toArray();
+    return result
+}
+
+const getVuelobyId= async (dbName, collectionName, id) => {
+    const idMongo = new ObjectId(id)
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.findOne({ _id: idMongo });
+    return result
+}
+
+const insertVuelo = async (dbName, collectionName, data) => {
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.insertOne(data);
+    return result
+}
+
+const updateVueloById = async (dbName, collectionName, { id, data }) => {
+    const mongoClient = new MongoClient(uri)
+    const idMongo = new ObjectId(id)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    delete data._id
+    const result = await collection.replaceOne({ _id: idMongo }, data);
+    return result
+}
+
+const deleteVueloById = async (dbName, collectionName, id) => {
+    const idMongo = new ObjectId(id)
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.deleteOne({ _id: idMongo });
+    return result
+}
+
+module.exports = {getVuelos,getVuelobyId,insertVuelo,updateVueloById,deleteVueloById}
+
+
+//Bandido Reservas
+const getReservas = async (dbName, collectionName) => {
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.find({}).toArray();
+    return result
+}
+
+const getReservabyId= async (dbName, collectionName, id) => {
+    const idMongo = new ObjectId(id)
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.findOne({ _id: idMongo });
+    return result
+}
+
+const insertReserva = async (dbName, collectionName, data) => {
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.insertOne(data);
+    return result
+}
+
+const updateReservaById = async (dbName, collectionName, { id, data }) => {
+    const mongoClient = new MongoClient(uri)
+    const idMongo = new ObjectId(id)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    delete data._id
+    const result = await collection.replaceOne({ _id: idMongo }, data);
+    return result
+}
+
+const deleteReservaById = async (dbName, collectionName, id) => {
+    const idMongo = new ObjectId(id)
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.deleteOne({ _id: idMongo });
+    return result
+}
+
+module.exports = {getReservas,getReservabyId,insertReserva,updateReservaById,deleteReservaById}
+
+
+//Usuarios Emanuel
+
+const getUsuarios = async (dbName, collectionName) => {
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.find({}).toArray();
+    return result
+}
+
+const getUsuariobyId= async (dbName, collectionName, id) => {
+    const idMongo = new ObjectId(id)
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.findOne({ _id: idMongo });
+    return result
+}
+
+const insertUsuario = async (dbName, collectionName, data) => {
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.insertOne(data);
+    return result
+}
+
+const updateUsuarioById = async (dbName, collectionName, { id, data }) => {
+    const mongoClient = new MongoClient(uri)
+    const idMongo = new ObjectId(id)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    delete data._id
+    const result = await collection.replaceOne({ _id: idMongo }, data);
+    return result
+}
+
+const deleteUsuarioById = async (dbName, collectionName, id) => {
+    const idMongo = new ObjectId(id)
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.deleteOne({ _id: idMongo });
+    return result
+}
+
+module.exports = {getUsuarios,getUsuariobyId,insertUsuario,updateUsuarioById,deleteUsuarioById}
+
+//Luna
+
+const getAviones = async (dbName, collectionName) => {
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.find({}).toArray();
+    return result
+}
+
+const getAvionbyId= async (dbName, collectionName, id) => {
+    const idMongo = new ObjectId(id)
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.findOne({ _id: idMongo });
+    return result
+}
+
+const insertAvion = async (dbName, collectionName, data) => {
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.insertOne(data);
+    return result
+}
+
+const updateAvionById = async (dbName, collectionName, { id, data }) => {
+    const mongoClient = new MongoClient(uri)
+    const idMongo = new ObjectId(id)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    delete data._id
+    const result = await collection.replaceOne({ _id: idMongo }, data);
+    return result
+}
+
+const deleteAvionById = async (dbName, collectionName, id) => {
+    const idMongo = new ObjectId(id)
+    const mongoClient = new MongoClient(uri)
+    const db = mongoClient.db(dbName)
+    const collection = db.collection(collectionName)
+    const result = await collection.deleteOne({ _id: idMongo });
+    return result
+}
+
+module.exports = {getAviones,getAvionbyId,insertAvion,updateAvionById,deleteAvionById}
---
+++ desarrollo-web-frontend-TravelbySasha-nuxtjs/src/routes/routes.js
@@ -0,0 +1,56 @@
+import DashboardLayout from '../layout/DashboardLayout.vue'
+// GeneralViews
+import NotFound from '../pages/NotFoundPage.vue'
+
+// Admin pages
+import Overview from 'src/pages/Overview.vue'
+import UserProfile from 'src/pages/UserProfile.vue'
+import TableList from 'src/pages/TableList.vue'
+import Typography from 'src/pages/Typography.vue'
+import Icons from 'src/pages/Icons.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    component: DashboardLayout,
+    redirect: '/admin/overview'
+  },
+  {
+    path: '/admin',
+    component: DashboardLayout,
+    redirect: '/admin/overview',
+    children: [
+      {
+        path: 'overview',
+        name: 'Overview',
+        component: Overview
+      },
+      {
+        path: 'user',
+        name: 'User',
+        component: UserProfile
+      },
+      {
+        path: 'table-list',
+        name: 'Table List',
+        component: TableList
+      },
+      {
+        path: 'typography',
+        name: 'Typography',
+        component: Typography
+      },
+      {
+        path: 'icons',
+        name: 'Icons',
+        component: Icons
+      },
+    ]
+  },
+  { path: '*', component: NotFound }
+]
+
+
+
+export default routes
