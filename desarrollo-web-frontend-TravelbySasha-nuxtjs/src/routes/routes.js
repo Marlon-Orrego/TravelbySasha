d35f4292f2c266269787891578b9e06b5e9f2e50
Juansebaslg <juansebassam@gmail.com>
2022-10-24 21:08:32 -0500

    Actualización front y diseño e implementación de login

--- desarrollo-web-frontend-TravelbySasha-nuxtjs/src/routes/routes.js
+++ desarrollo-web-frontend-TravelbySasha-nuxtjs/src/routes/routes.js
@@ -1,56 +1,41 @@
-import DashboardLayout from '../layout/DashboardLayout.vue'
+import DashboardLayout from "../layout/DashboardLayout.vue";
 // GeneralViews
-import NotFound from '../pages/NotFoundPage.vue'
+import NotFound from "../pages/NotFoundPage.vue";
 
 // Admin pages
-import Overview from 'src/pages/Overview.vue'
-import UserProfile from 'src/pages/UserProfile.vue'
-import TableList from 'src/pages/TableList.vue'
-import Typography from 'src/pages/Typography.vue'
-import Icons from 'src/pages/Icons.vue'
-
+import Overview from "src/pages/Overview.vue";
+import UserProfile from "src/pages/UserProfile.vue";
+import Login from "src/pages/Login.vue";
 
 const routes = [
   {
-    path: '/',
-    component: DashboardLayout,
-    redirect: '/admin/overview'
+    path: "/",
+    name: "login",
+    component: Login,
   },
   {
-    path: '/admin',
+    path: "/home",
     component: DashboardLayout,
-    redirect: '/admin/overview',
+    redirect: "/admin/overview",
+  },
+  {
+    path: "/admin",
+    component: DashboardLayout,
+    redirect: "/admin/overview",
     children: [
       {
-        path: 'overview',
-        name: 'Overview',
-        component: Overview
+        path: "overview",
+        name: "Overview",
+        component: Overview,
       },
       {
-        path: 'user',
-        name: 'User',
-        component: UserProfile
+        path: "user",
+        name: "User",
+        component: UserProfile,
       },
-      {
-        path: 'table-list',
-        name: 'Table List',
-        component: TableList
-      },
-      {
-        path: 'typography',
-        name: 'Typography',
-        component: Typography
-      },
-      {
-        path: 'icons',
-        name: 'Icons',
-        component: Icons
-      },
-    ]
+    ],
   },
-  { path: '*', component: NotFound }
-]
+  //{ path: '*', component: NotFound }
+];
 
-
-
-export default routes
+export default routes;
